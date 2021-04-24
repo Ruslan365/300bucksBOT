8733e79d5776a88acc31c92372a74daf1481004e
Nikita <aeromusician@yandex.ru>
2021-04-24 21:50:27 +0300

    Initial commit
    Downgrade

---
+++ config.py
@@ -0,0 +1,4 @@
+token = '1665086745:AAHN151UBrGp5L36SjKAJxPjGt1aKmxY9dk'
+API_KEY = 'fcf1bfc080c24b10848a9a286d15c69c'
+url_old = f'http://api.currencylayer.com/live?access_key={API_KEY}'
+url = 'https://www.cbr-xml-daily.ru/latest.js'
