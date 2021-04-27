2811c8d998a8b93e82599372bba6ed8b33e37fba
Nikita <aeromusician@yandex.ru>
2021-04-27 17:21:54 +0300

    Удалил всякую хрень

--- bot.py
+++ bot.py
@@ -11,13 +11,30 @@ responce = requests.get(config.url)
 val = json.loads(responce.text)
 
 
+def is_number(message):
+    try:
+        float(message.text)
+        return True
+    except ValueError:
+        return False
+
+
+
+
+
+
 
 @bot.message_handler(commands=["start"])
 def get_text_messages(message):
     bot.send_message(message.from_user.id, "Доброго времени суток, выберете желаемую функцию.", reply_markup=keyboard1)
 
-print("HI")
 
+def reccount(message):
+    if is_number(message):
+        bot.send_message(message.chat.id, "work1")
+    else:
+        bot.send_message(message.chat.id, "work")
+        #она не должна запускаться пока прользватель не введет сообщение
 
 
 @bot.message_handler(content_types=['text'])
@@ -25,21 +42,11 @@ def Check_valute(message):
     if message.text == "Узнать курс валют":
         bot.send_message(message.chat.id, "Курс Евро: 1 EUR = " + str(round(1/(val["rates"]["EUR"]), 3)) + " рублей")
         bot.send_message(message.chat.id, "Курс Доллара: 1 USD = " + str(round(1/(val["rates"]["USD"]), 3)) + " рублей")
-
     else:
         bot.send_message(message.chat.id, "Введите значение в рублях: ")
-        '''bot.send_message(message.chat.id, "Рубль - > ЕВРО: " + str(resEUR)) 
-        bot.send_message(message.chat.id, "Рубль - > ДОЛЛАР: " + str(resUSD))
-'''
+        #вот тут функция reccount вызывается
 
-'''def recount(message):     Функция пересчета валюты(не робит)
-    EUR = float(1/(val["rates"]["EUR"]))
-    USD = float(1/(val["rates"]["USD"]))
-    RUB = float(message.text)
-    resEUR = round((RUB * EUR), 3)
-    resUSD = round((RUB * USD), 3)
-    return resEUR, resUSD
-'''
+
 
 
 if __name__ == '__main__':
